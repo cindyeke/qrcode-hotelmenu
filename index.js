952b2b438c6b7bdbbc76ea0a67448d762d6ed0c2
cindyeke <officialcee1@gmail.com>
2021-06-10 17:03:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8100;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", require("./router/routes"));
+
+app.listen(PORT, () => {
+  console.log("Hotel QR Generator running on PORT " + PORT);
+});
